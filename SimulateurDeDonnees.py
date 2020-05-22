6d62ef6f50916e9b938b5dda0b721a4094e64603
Golden28 <le.magicien.d.oz@gmail.com>
2020-05-22 19:32:42 +0200

    Update SimulateurDeDonnees.py

--- SimulateurDeDonnees.py
+++ SimulateurDeDonnees.py
@@ -20,9 +20,9 @@ def transformer_listedenombre_en_chaine(liste):
 ##### Programme principal
 ### Réglages
 R = 25  # Valeur de la résistance utilisé pour le tp
-Lugéné = [0.0, 3.0, 4.5, 6.0, 7.5, 9.0, 12.0]  # Listes des valeurs indiqué sur le générateur de tension prévue pour le tp
+Lugene = [0.0, 3.0, 4.5, 6.0, 7.5, 9.0, 12.0]  # Listes des valeurs indiqué sur le générateur de tension prévue pour le tp
 
-U = [u + gauss(0, 0.05) for u in Lugéné]  # Ajout d'une erreur aléatoire de type distribution gaussienne aux valeurs affichées sur l'appareil
+U = [u + gauss(0, 0.05) for u in Lugene]  # Ajout d'une erreur aléatoire de type distribution gaussienne aux valeurs affichées sur l'appareil
 I = [u / R + gauss(0, 0.015) for u in U]  # Ajout d'une erreur aléatoire de type distribution gaussienne aux valeurs de I
 
 # Forcage du premier point à 0,0
